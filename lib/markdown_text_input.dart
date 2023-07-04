6f791b015d2e7484905c2592be9cd0658547a66e
codingsenpi <bagdi.rahul2004@gmail.com>
2023-07-04 11:06:13 +0530

    added hintStyle, boxConstraints and widgetHeight

--- lib/markdown_text_input.dart
+++ lib/markdown_text_input.dart
@@ -40,12 +40,23 @@ class MarkdownTextInput extends StatefulWidget {
   /// Default value is true.
   final bool insertLinksByDialog;
 
+  /// Give TextStyle to the hintText
+  final TextStyle? hintStyle;
+
+  /// BoxConstraints for the widget body
+  final BoxConstraints? boxConstraints;
+
+  /// height for the main widget body
+  final double? widgetHeight;
+
   /// Constructor for [MarkdownTextInput]
   MarkdownTextInput(
     this.onTextChanged,
     this.initialValue, {
     this.label = '',
     this.validators,
+    this.widgetHeight,
+    this.boxConstraints,
     this.textDirection = TextDirection.ltr,
     this.maxLines = 10,
     this.actions = const [
@@ -55,37 +66,49 @@ class MarkdownTextInput extends StatefulWidget {
       MarkdownType.link,
       MarkdownType.list
     ],
+    this.hintStyle,
     this.textStyle,
     this.controller,
     this.insertLinksByDialog = true,
   });
 
   @override
-  _MarkdownTextInputState createState() => _MarkdownTextInputState(controller ?? TextEditingController());
+  _MarkdownTextInputState createState() =>
+      _MarkdownTextInputState(controller ?? TextEditingController());
 }
 
 class _MarkdownTextInputState extends State<MarkdownTextInput> {
   final TextEditingController _controller;
-  TextSelection textSelection = const TextSelection(baseOffset: 0, extentOffset: 0);
+  TextSelection textSelection =
+      const TextSelection(baseOffset: 0, extentOffset: 0);
   FocusNode focusNode = FocusNode();
 
   _MarkdownTextInputState(this._controller);
 
-  void onTap(MarkdownType type, {int titleSize = 1, String? link, String? selectedText}) {
+  void onTap(MarkdownType type,
+      {int titleSize = 1, String? link, String? selectedText}) {
     final basePosition = textSelection.baseOffset;
-    var noTextSelected = (textSelection.baseOffset - textSelection.extentOffset) == 0;
+    var noTextSelected =
+        (textSelection.baseOffset - textSelection.extentOffset) == 0;
 
     var fromIndex = textSelection.baseOffset;
     var toIndex = textSelection.extentOffset;
 
-    final result = FormatMarkdown.convertToMarkdown(type, _controller.text, fromIndex, toIndex,
-        titleSize: titleSize, link: link, selectedText: selectedText ?? _controller.text.substring(fromIndex, toIndex));
+    final result = FormatMarkdown.convertToMarkdown(
+        type, _controller.text, fromIndex, toIndex,
+        titleSize: titleSize,
+        link: link,
+        selectedText:
+            selectedText ?? _controller.text.substring(fromIndex, toIndex));
 
-    _controller.value = _controller.value
-        .copyWith(text: result.data, selection: TextSelection.collapsed(offset: basePosition + result.cursorIndex));
+    _controller.value = _controller.value.copyWith(
+        text: result.data,
+        selection:
+            TextSelection.collapsed(offset: basePosition + result.cursorIndex));
 
     if (noTextSelected) {
-      _controller.selection = TextSelection.collapsed(offset: _controller.selection.end - result.replaceCursorIndex);
+      _controller.selection = TextSelection.collapsed(
+          offset: _controller.selection.end - result.replaceCursorIndex);
       focusNode.requestFocus();
     }
   }
@@ -94,7 +117,8 @@ class _MarkdownTextInputState extends State<MarkdownTextInput> {
   void initState() {
     _controller.text = widget.initialValue;
     _controller.addListener(() {
-      if (_controller.selection.baseOffset != -1) textSelection = _controller.selection;
+      if (_controller.selection.baseOffset != -1)
+        textSelection = _controller.selection;
       widget.onTextChanged(_controller.text);
     });
     super.initState();
@@ -110,9 +134,12 @@ class _MarkdownTextInputState extends State<MarkdownTextInput> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: widget.widgetHeight,
+      constraints: widget.boxConstraints,
       decoration: BoxDecoration(
         color: Theme.of(context).cardColor,
-        border: Border.all(color: Theme.of(context).colorScheme.secondary, width: 2),
+        border: Border.all(
+            color: Theme.of(context).colorScheme.secondary, width: 2),
         borderRadius: const BorderRadius.all(Radius.circular(10)),
       ),
       child: Column(
@@ -123,25 +150,32 @@ class _MarkdownTextInputState extends State<MarkdownTextInput> {
             maxLines: widget.maxLines,
             controller: _controller,
             textCapitalization: TextCapitalization.sentences,
-            validator: widget.validators != null ? (value) => widget.validators!(value) : null,
-            style: widget.textStyle ?? Theme.of(context).textTheme.bodyText1,
+            validator: widget.validators != null
+                ? (value) => widget.validators!(value)
+                : null,
+            style: widget.textStyle ?? Theme.of(context).textTheme.bodyLarge,
             cursorColor: Theme.of(context).primaryColor,
             textDirection: widget.textDirection,
             decoration: InputDecoration(
-              enabledBorder:
-                  UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).colorScheme.secondary)),
-              focusedBorder:
-                  UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).colorScheme.secondary)),
+              enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                      color: Theme.of(context).colorScheme.secondary)),
+              focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                      color: Theme.of(context).colorScheme.secondary)),
               hintText: widget.label,
-              hintStyle: const TextStyle(color: Color.fromRGBO(63, 61, 86, 0.5)),
-              contentPadding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+              hintStyle: widget.hintStyle,
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
             ),
           ),
           SizedBox(
             height: 44,
             child: Material(
               color: Theme.of(context).cardColor,
-              borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(10), bottomRight: Radius.circular(10)),
+              borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10)),
               child: ListView(
                 scrollDirection: Axis.horizontal,
                 children: widget.actions.map((type) {
@@ -156,7 +190,9 @@ class _MarkdownTextInputState extends State<MarkdownTextInput> {
                                 padding: EdgeInsets.all(10),
                                 child: Text(
                                   'H#',
-                                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w700),
                                 ),
                               ),
                             ),
@@ -168,12 +204,15 @@ class _MarkdownTextInputState extends State<MarkdownTextInput> {
                                 for (int i = 1; i <= 6; i++)
                                   InkWell(
                                     key: Key('H${i}_button'),
-                                    onTap: () => onTap(MarkdownType.title, titleSize: i),
+                                    onTap: () =>
+                                        onTap(MarkdownType.title, titleSize: i),
                                     child: Padding(
                                       padding: const EdgeInsets.all(10),
                                       child: Text(
                                         'H$i',
-                                        style: TextStyle(fontSize: (18 - i).toDouble(), fontWeight: FontWeight.w700),
+                                        style: TextStyle(
+                                            fontSize: (18 - i).toDouble(),
+                                            fontWeight: FontWeight.w700),
                                       ),
                                     ),
                                   ),
@@ -196,25 +235,31 @@ class _MarkdownTextInputState extends State<MarkdownTextInput> {
                         customOnTap: !widget.insertLinksByDialog
                             ? null
                             : () async {
-                                var text =
-                                    _controller.text.substring(textSelection.baseOffset, textSelection.extentOffset);
+                                var text = _controller.text.substring(
+                                    textSelection.baseOffset,
+                                    textSelection.extentOffset);
 
-                                var textController = TextEditingController()..text = text;
+                                var textController = TextEditingController()
+                                  ..text = text;
                                 var linkController = TextEditingController();
                                 var textFocus = FocusNode();
                                 var linkFocus = FocusNode();
 
-                                var color = Theme.of(context).colorScheme.secondary;
-                                var language =
-                                    kIsWeb ? window.locale.languageCode : Platform.localeName.substring(0, 2);
+                                var color =
+                                    Theme.of(context).colorScheme.secondary;
+                                var language = kIsWeb
+                                    ? window.locale.languageCode
+                                    : Platform.localeName.substring(0, 2);
 
                                 var textLabel = 'Text';
                                 var linkLabel = 'Link';
                                 try {
-                                  var textTranslation = await GoogleTranslator().translate(textLabel, to: language);
+                                  var textTranslation = await GoogleTranslator()
+                                      .translate(textLabel, to: language);
                                   textLabel = textTranslation.text;
 
-                                  var linkTranslation = await GoogleTranslator().translate(linkLabel, to: language);
+                                  var linkTranslation = await GoogleTranslator()
+                                      .translate(linkLabel, to: language);
                                   linkLabel = linkTranslation.text;
                                 } catch (e) {
                                   textLabel = 'Text';
@@ -226,10 +271,13 @@ class _MarkdownTextInputState extends State<MarkdownTextInput> {
                                     builder: (context) {
                                       return AlertDialog(
                                         title: Row(
-                                          mainAxisAlignment: MainAxisAlignment.end,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.end,
                                           children: [
                                             GestureDetector(
-                                                child: Icon(Icons.close), onTap: () => Navigator.pop(context))
+                                                child: Icon(Icons.close),
+                                                onTap: () =>
+                                                    Navigator.pop(context))
                                           ],
                                         ),
                                         content: Column(
@@ -238,44 +286,66 @@ class _MarkdownTextInputState extends State<MarkdownTextInput> {
                                             TextField(
                                               controller: textController,
                                               decoration: InputDecoration(
-                                                hintText: 'example',
+                                                hintText:
+                                                    'Checkout this really cool video',
                                                 label: Text(textLabel),
-                                                labelStyle: TextStyle(color: color),
+                                                labelStyle:
+                                                    TextStyle(color: color),
                                                 focusedBorder:
-                                                    OutlineInputBorder(borderSide: BorderSide(color: color, width: 2)),
+                                                    OutlineInputBorder(
+                                                        borderSide: BorderSide(
+                                                            color: color,
+                                                            width: 2)),
                                                 enabledBorder:
-                                                    OutlineInputBorder(borderSide: BorderSide(color: color, width: 2)),
+                                                    OutlineInputBorder(
+                                                        borderSide: BorderSide(
+                                                            color: color,
+                                                            width: 2)),
                                               ),
                                               autofocus: text.isEmpty,
                                               focusNode: textFocus,
-                                              textInputAction: TextInputAction.next,
+                                              textInputAction:
+                                                  TextInputAction.next,
                                               onSubmitted: (value) {
                                                 textFocus.unfocus();
-                                                FocusScope.of(context).requestFocus(linkFocus);
+                                                FocusScope.of(context)
+                                                    .requestFocus(linkFocus);
                                               },
                                             ),
                                             SizedBox(height: 10),
                                             TextField(
                                               controller: linkController,
                                               decoration: InputDecoration(
-                                                hintText: 'https://example.com',
+                                                hintText:
+                                                    'https://youtu.be/dQw4w9WgXcQ',
                                                 label: Text(linkLabel),
-                                                labelStyle: TextStyle(color: color),
+                                                labelStyle:
+                                                    TextStyle(color: color),
                                                 focusedBorder:
-                                                    OutlineInputBorder(borderSide: BorderSide(color: color, width: 2)),
+                                                    OutlineInputBorder(
+                                                        borderSide: BorderSide(
+                                                            color: color,
+                                                            width: 2)),
                                                 enabledBorder:
-                                                    OutlineInputBorder(borderSide: BorderSide(color: color, width: 2)),
+                                                    OutlineInputBorder(
+                                                        borderSide: BorderSide(
+                                                            color: color,
+                                                            width: 2)),
                                               ),
                                               autofocus: text.isNotEmpty,
                                               focusNode: linkFocus,
                                             ),
                                           ],
                                         ),
-                                        contentPadding: EdgeInsets.fromLTRB(24.0, 20.0, 24.0, 0),
+                                        contentPadding: EdgeInsets.fromLTRB(
+                                            24.0, 20.0, 24.0, 0),
                                         actions: [
                                           TextButton(
                                             onPressed: () {
-                                              onTap(type, link: linkController.text, selectedText: textController.text);
+                                              onTap(type,
+                                                  link: linkController.text,
+                                                  selectedText:
+                                                      textController.text);
                                               Navigator.pop(context);
                                             },
                                             child: const Text('OK'),
